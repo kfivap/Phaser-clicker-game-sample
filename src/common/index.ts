79067e5960a41004ad97464766719d11a641b6f0
kfivap <kfivap@gmail.com>
2024-03-24 20:42:04 +0500

    .

--- src/common/index.ts
+++
@@ -1,8 +0,0 @@
-export const defaultFontStyle = (wordWrapWidth: number)=>({
-    fontFamily: 'Roboto',
-    color: 'black',
-    fontSize: 30,
-    wordWrap: { width: wordWrapWidth }
-})
-
-export const defaultIndent = 20
